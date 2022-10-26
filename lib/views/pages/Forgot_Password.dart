12e6dc17a8c82fef1a6f056ab9c45572dd132105
Faisal-style <faisalrezarahmat1@gmail.com>
2022-10-26 08:10:28 +0700

    feat: add forgot password screen

---
+++ lib/views/pages/Forgot_Password.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class forgotpassword extends StatelessWidget {
+  const forgotpassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Poppins'),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: GestureDetector(
+            onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+            child: Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(32, 0, 32, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  //logo
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 35.8, 0, 0),
+                    child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("assets/images/itc.png")),
+                      ),
+                    ),
+                  ),
+
+                  const Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
+                    child: Text(
+                      "ITC Repository",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF01575C),
+                          fontSize: 28),
+                    ),
+                  ),
+
+                  const Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+                    child: Text(
+                      "Lupa Kata Sandi",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                          fontSize: 20),
+                    ),
+                  ),
+
+                  const Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
+                    child: Text(
+                      "Email",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                    child: SizedBox(
+                      height: 44,
+                      child: TextFormField(
+                        style: TextStyle(
+                            fontSize: 16, color: Color.fromRGBO(1, 87, 92, 1)),
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0x00000000)),
+                              borderRadius: BorderRadius.circular(6)),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Color(0x00000000))),
+                          hintText: "Masukkan Alamat Email",
+                          hintStyle: TextStyle(
+                              color: Color.fromRGBO(1, 87, 92, 1),
+                              fontSize: 16),
+                          filled: true,
+                          fillColor: Color.fromRGBO(217, 230, 231, 1),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+                    child: SizedBox(
+                      height: 44,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("Kirim"),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromRGBO(1, 87, 92, 1),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(6))),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Kembali ke Halaman Masuk",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Color.fromRGBO(1, 87, 92, 1),
+                            fontSize: 16),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
