927df15a09aed41da7b748d55c74f050c249f6aa
Kavarera <rafli.030715@gmail.com>
2022-10-27 12:41:32 +0700

    Implement getx in dropdown at signup screen

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Poppins',
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(),
+      home: SignupScreen_NoGetx(),
     );
   }
 } 
