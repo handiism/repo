8d6ee198554e9b0ba0e9a355cb6e70ad09cf6086
handiism <mail.handira@gmail.com>
2022-11-04 13:30:25 +0700

    feat: import in one file

---
+++ lib/views/screens/screens.dart
@@ -0,0 +1,3 @@
+export 'package:repo/views/screens/forgot_pass_screen.dart';
+export 'package:repo/views/screens/login_screen.dart';
+export 'package:repo/views/screens/sign_up_screen.dart';
---
+++ lib/views/widgets/widgets.dart
@@ -0,0 +1,3 @@
+export 'package:repo/views/widgets/button_widget.dart';
+export 'package:repo/views/widgets/snackbar_widget.dart';
+export 'package:repo/views/widgets/text_field_widget.dart';
