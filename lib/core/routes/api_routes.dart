52febd3f5b7cc22608060f886824bac13a38a638
Faisal-style <faisalrezarahmat1@gmail.com>
2022-11-06 15:55:27 +0700

    add route for forgot password

--- lib/core/routes/api_routes.dart
+++ lib/core/routes/api_routes.dart
@@ -1,4 +1,5 @@
 abstract class ApiRoutesRepo {
-  static const String baseUrl = 'http://34.101.216.127:3000';
+  static const String baseUrl = 'http://192.168.100.234:3000';
   static const String login = '/user/login';
+  static const String forgot = '/password-reset';
 }
