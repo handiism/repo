1785f8d67abaec48e104e5d11c40b74433f69c8b
Iman Abdurrahman <72773420+kentangtelo@users.noreply.github.com>
2022-11-07 01:08:55 +0700

    fix: Benerin login controller

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,17 +3,12 @@ import 'package:repo/core/routes/routes.dart';
 import 'package:repo/models/user/user.dart';
 
 class ApiService extends GetConnect implements GetxService {
-  Future<UserLoginResponse> login(String uri, UserLoginRequest body) async {
+  Future<Response> login(String uri, UserLoginRequest body) async {
     Response response = await post(
       ApiRoutesRepo.baseUrl + uri,
       body.toJson(),
       headers: {'Content-Type': 'application/json; charset=UTF-8'},
     );
-
-    if (response.statusCode != 200) {
-      throw Error();
-    }
-
-    return UserLoginResponse.fromJson(response.body['data']['user']);
+    return response;
   }
 }
