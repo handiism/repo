b53576d418a087e361378c5e384bcadd30701553
Iman Abdurrahman <72773420+kentangtelo@users.noreply.github.com>
2022-11-27 02:05:14 +0700

    fix: ui slicing homescreen

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:repo/core/shared/colors.dart';
 import 'package:repo/core/utils/formatting.dart';
@@ -10,6 +11,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isDescending = true;
+  List<String> divisi = <String>[
+    'Semua',
+    'Web',
+    'Mobile',
+    'PM',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,14 +36,97 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: const EdgeInsets.only(right: 30),
             onPressed: () {},
             icon: const Icon(
-              Icons.search_rounded,
+              Icons.search_outlined,
             ),
           )
         ],
       ),
-      body: const Center(
-        child: Text(
-          'Homescreen',
+      body: Container(
+        padding: const EdgeInsets.only(
+          top: 20,
+          left: 20,
+          right: 20,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Materi',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  height: 40,
+                  width: MediaQuery.of(context).size.width / 2.3,
+                  child: DropdownSearch<String>(
+                    dropdownButtonProps: const DropdownButtonProps(
+                      color: Colors.white,
+                      icon: Icon(Icons.keyboard_arrow_down),
+                    ),
+                    dropdownDecoratorProps: DropDownDecoratorProps(
+                      dropdownSearchDecoration: InputDecoration(
+                        filled: true,
+                        fillColor: hexToColor(ColorsRepo.primaryColor),
+                        contentPadding:
+                            const EdgeInsets.fromLTRB(12, 10, 10, 10),
+                      ),
+                    ),
+                    items: divisi,
+                    dropdownBuilder: (context, selectedItem) {
+                      return Text(
+                        selectedItem ?? 'Divisi',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                  width: MediaQuery.of(context).size.width / 2.3,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: hexToColor(ColorsRepo.primaryColor),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          isDescending ? 'A-Z' : 'Z-A',
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        Icon(
+                          isDescending
+                              ? Icons.keyboard_arrow_down
+                              : Icons.keyboard_arrow_up,
+                          size: 24.0,
+                        )
+                      ],
+                    ),
+                    onPressed: () =>
+                        setState(() => isDescending = !isDescending),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
