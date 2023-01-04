39117ae1e015f5b85f54d53f6927b8048ff1c589
handiism <mail.handira@gmail.com>
2023-01-04 23:36:46 +0700

    fix: base api route

--- lib/core/routes/api_routes.dart
+++ lib/core/routes/api_routes.dart
@@ -1,5 +1,5 @@
 abstract class ApiRoutesRepo {
-  static const String baseUrl = 'http://192.168.8.100:3000';
+  static const String baseUrl = 'http://34.101.216.127:3000/';
   static const String login = '/user/login';
   static const String forgot = '/password-reset';
   static const String register = '/user/register';
